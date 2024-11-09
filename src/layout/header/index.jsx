71f4ff4443a25408b1c280348b4f44915d8eb167
samira <samira.mousaie@divar.ir>
2024-11-09 15:06:19 +0330

    add localization

--- src/layout/header/index.jsx
+++ src/layout/header/index.jsx
@@ -1,18 +1,33 @@
 import icon from '../../assets/images/icon.svg'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+
+import { t } from "i18next";
+
 function Header() {
   return (
     <header>
-      <ul>
-        <li>
-          <img alt="bitPin" src={icon} />
-        </li>
-        <li>خرید آسان</li>
-        <li>معامله</li>
-        <li>قیمت ارز دیجیتال</li>
-        <li>امکانات</li>
-        <li>آموزش</li>
-      </ul>
+      <div>
+        <ul>
+          <li>
+            <img alt="bitPin" src={icon} />
+          </li>
+          <li>{t("layout.header.easyPurchase")}</li>
+          <li>{t("layout.header.trade")}</li>
+          <li>{t("layout.header.cryptoCurrencyPrice")}</li>
+          <li>{t("layout.header.features")}</li>
+          <li>{t("layout.header.education")}</li>
+        </ul>
+      </div>
+      <div>
+        <a href="#" target="_blank">
+          <FontAwesomeIcon icon={faMoon} />
+        </a>
+        <a href="#" target="_blank">
+          <FontAwesomeIcon icon={faSun} />
+        </a>
+      </div>
     </header>
   )
 }
